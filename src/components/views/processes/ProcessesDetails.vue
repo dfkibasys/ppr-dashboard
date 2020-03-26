addbc33f76d1b590ef1eab015c3b535766dbe846
jomo02 <jonas.mohr@dfki.de>
2020-03-26 10:33:53 +0100

    details styling

--- src/components/views/processes/ProcessesDetails.vue
+++ src/components/views/processes/ProcessesDetails.vue
@@ -1,67 +1,65 @@
 <template>
   <b-container fluid>
-    <b-row>
-      <b-col class="leftDetails">
+    <b-row class="pb-2">
+      <b-col class="leftDetails pl-2 border">
         <div class="button" v-show="ctrl.showLeftDetails">
-          <b-button @click="ctrl.showLeftDetails = !ctrl.showLeftDetails">
+          <b-button variant="light" @click="ctrl.showLeftDetails = !ctrl.showLeftDetails">
             <b-icon-chevron-left font-scale="1"></b-icon-chevron-left>
           </b-button>
         </div>
         <b-list-group>
-          <b-list-group-item>
-            <div class="md-list-item-text" layout="column">
-              <h5>Definition Version:</h5>
-              <p>
-                <b-form-select v-model="ctrl.currentVersionID" :options="ctrl.versions"></b-form-select>
-              </p>
-            </div>
+          <b-list-group-item class="border-0">
+            <h5 class="mb-0">Definition Version:</h5>
+            <p class="mb-0">
+              <b-form-select v-model="ctrl.currentVersionID" :options="ctrl.versions"></b-form-select>
+            </p>
           </b-list-group-item>
-          <b-list-group-item>
-            <h5 class="mb-1">Version Tag:</h5>
-            <p>{{ctrl.processDefinition.versionTag || "null"}}</p>
+          <b-list-group-item class="border-0">
+            <h5 class="mb-0">Version Tag:</h5>
+            <p class="mb-0">{{ctrl.processDefinition.versionTag || "null"}}</p>
           </b-list-group-item>
-          <b-list-group-item>
-            <h5>Definition ID:</h5>
-            <p>{{ctrl.processDefinition.id || "-"}}</p>
+          <b-list-group-item class="border-0">
+            <h5 class="mb-0">Definition ID:</h5>
+            <p class="mb-0">{{ctrl.processDefinition.id || "-"}}</p>
           </b-list-group-item>
-          <b-list-group-item>
-            <h5>Definition Key:</h5>
-            <p>{{ctrl.processDefinition.key || "-"}}</p>
+          <b-list-group-item class="border-0">
+            <h5 class="mb-0">Definition Key:</h5>
+            <p class="mb-0">{{ctrl.processDefinition.key || "-"}}</p>
           </b-list-group-item>
-          <b-list-group-item>
-            <h5>Definition Name:</h5>
-            <p>{{ctrl.processDefinition.name || "-"}}</p>
+          <b-list-group-item class="border-0">
+            <h5 class="mb-0">Definition Name:</h5>
+            <p class="mb-0">{{ctrl.processDefinition.name || "-"}}</p>
           </b-list-group-item>
-          <b-list-group-item>
-            <h5>Tenant ID:</h5>
-            <p>{{ctrl.processDefinition.tenantId || "null"}}</p>
+          <b-list-group-item class="border-0">
+            <h5 class="mb-0">Tenant ID:</h5>
+            <p class="mb-0">{{ctrl.processDefinition.tenantId || "null"}}</p>
           </b-list-group-item>
-          <b-list-group-item>
-            <h5>Deployment ID:</h5>
-            <p>{{ctrl.processDefinition.deploymentId || "-"}}</p>
+          <b-list-group-item class="border-0">
+            <h5 class="mb-0">Deployment ID:</h5>
+            <p class="mb-0">{{ctrl.processDefinition.deploymentId || "-"}}</p>
           </b-list-group-item>
-          <b-list-group-item>
-            <h5>Instances Running:</h5>
-            <p>{{ctrl.processDefinition.instances || "0"}}</p>
+          <b-list-group-item class="border-0">
+            <h5 class="mb-0">Instances Running:</h5>
+            <p class="mb-0">{{ctrl.processDefinition.instances || "0"}}</p>
           </b-list-group-item>
         </b-list-group>
       </b-col>
 
-      <b-col>
+      <b-col class="border">
         <div v-show="!ctrl.showLeftDetails" style="position: absolute;left: 0px;z-index: 9999;">
-          <b-button @click="ctrl.showLeftDetails = !ctrl.showLeftDetails">
+          <b-button variant="light" @click="ctrl.showLeftDetails = !ctrl.showLeftDetails">
             <b-icon-chevron-right font-scale="1"></b-icon-chevron-right>
           </b-button>
         </div>
-        <div id="diagram-container">
+        <div id="diagram-container" >
           <vue-bpmn url="data/diagram.bpmn" v-on:error="handleError" v-on:shown="handleShown"></vue-bpmn>
         </div>
       </b-col>
     </b-row>
-    <b-row>
+    <b-row class="px-2">
       <b-col>
         <b-tabs>
-          <b-tab title="process instances">
+          <b-tab title="Process Instances">
             <b-table
               hover
               striped
@@ -69,7 +67,7 @@
               :items="ctrl.processInstances"
             ></b-table>
           </b-tab>
-          <b-tab title="audit log">
+          <b-tab title="Audit Log">
             <b-table hover striped :items="ctrl.auditlog"></b-table>
           </b-tab>
         </b-tabs>
@@ -136,14 +134,15 @@ export default {
 <style lang=less scoped>
 .leftDetails {
   position: relative;
-  background: rgb(245, 245, 245);
   color: rgba(0, 0, 0, 0.87);
   max-width: 20%;
-  width: 20%;
   .button {
     position: absolute;
     right: 0px;
     z-index: 9999;
   }
+}
+#diagram-container {
+  height: 100%;
 }
 </style>
