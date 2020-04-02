d84bf03e406a08383f6a622596422c97107950d5
jomo02 <jonas.mohr@dfki.de>
2020-04-02 14:39:56 +0200

    add process variables

--- src/components/modals/CreateProcessInstance.vue
+++ src/components/modals/CreateProcessInstance.vue
@@ -9,6 +9,39 @@
       placeholder="Enter Business Key"
     ></b-form-input>
     <b-form-invalid-feedback id="input-live-feedback">The business key is required.</b-form-invalid-feedback>
+
+    <div v-for="variable in processVariables" :key="variable.id">
+      <b-row>
+        <b-col>
+          <b-form-input v-model="variable.name" placeholder="Name"></b-form-input>
+        </b-col>
+        <b-col>
+          <b-form-select v-model="variable.type" :options="typeOptions"></b-form-select>
+        </b-col>
+        <b-col>
+          <b-form-input
+            v-if="variable.type === 'string'"
+            v-model="variable.value"
+            placeholder="Value"
+            :type="'text'"
+          ></b-form-input>
+          <b-form-input
+            v-else-if="variable.type === 'integer'"
+            v-model="variable.value"
+            placeholder="Value"
+            :type="'number'"
+          ></b-form-input>
+          <b-form-select
+            v-else-if="variable.type === 'boolean'"
+            v-model="variable.value"
+            :options="boolOptions"
+          ></b-form-select>
+          <b-form-input :disabled="true" v-else></b-form-input>
+        </b-col>
+      </b-row>
+    </div>
+    <b-button @click="addVariable">Add Variable</b-button>
+
     <template v-slot:modal-footer="{ok, cancel}">
       <b-button @click="cancel">Don't create</b-button>
       <b-button :disabled="!keyState" variant="success" @click="ok">Create</b-button>
@@ -23,7 +56,11 @@ export default {
   name: "CreateProcessInstance",
   data() {
     return {
-      businessKey: ""
+      businessKey: "",
+      processVariables: [],
+      processVariableID: 0,
+      typeOptions: ["boolean", "string", "integer"],
+      boolOptions: ["true", "false"]
     };
   },
   computed: {
@@ -32,6 +69,14 @@ export default {
     }
   },
   methods: {
+    addVariable() {
+      this.processVariables.push({
+        id: this.processVariableID++,
+        name: "",
+        type: "",
+        value: ""
+      });
+    },
     createInstance() {
       axios
         .post(
@@ -41,9 +86,7 @@ export default {
             "/start",
           {
             businessKey: this.businessKey,
-            variables: {
-                
-            }
+            variables: {}
           }
         )
         .then(res => {
