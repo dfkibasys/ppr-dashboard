410e2ea7422b2fe7a5fffe9ec487dfd061cae9d4
jomo02 <jonas.mohr@dfki.de>
2020-04-02 15:53:46 +0200

    CPI modal styling and var removing

--- src/components/modals/CreateProcessInstance.vue
+++ src/components/modals/CreateProcessInstance.vue
@@ -7,39 +7,44 @@
       required
       v-model="businessKey"
       placeholder="Enter Business Key"
+      class="mb-2"
     ></b-form-input>
-    <b-form-invalid-feedback id="input-live-feedback">The business key is required.</b-form-invalid-feedback>
+    <b-form-invalid-feedback id="input-live-feedback" class="mb-2">The business key is required.</b-form-invalid-feedback>
 
-    <div v-for="variable in processVariables" :key="variable.id">
-      <b-row>
-        <b-col>
-          <b-form-input v-model="variable.name" placeholder="Name"></b-form-input>
-        </b-col>
-        <b-col>
-          <b-form-select v-model="variable.type" :options="typeOptions"></b-form-select>
-        </b-col>
-        <b-col>
-          <b-form-input
-            v-if="variable.type === 'string'"
-            v-model="variable.value"
-            placeholder="Value"
-            :type="'text'"
-          ></b-form-input>
-          <b-form-input
-            v-else-if="variable.type === 'integer'"
-            v-model="variable.value"
-            placeholder="Value"
-            :type="'number'"
-          ></b-form-input>
-          <b-form-select
-            v-else-if="variable.type === 'boolean'"
-            v-model="variable.value"
-            :options="boolOptions"
-          ></b-form-select>
-          <b-form-input :disabled="true" v-else></b-form-input>
-        </b-col>
-      </b-row>
-    </div>
+      <b-container class="p-0">
+        <b-form-row class="mb-2" v-for="variable in processVariables" :key="variable.id">
+          <b-col>
+            <b-form-input v-model="variable.name" placeholder="Name"></b-form-input>
+          </b-col>
+          <b-col>
+            <b-form-select v-model="variable.type" :options="typeOptions"></b-form-select>
+          </b-col>
+          <b-col>
+            <b-form-input
+              v-if="variable.type === 'string'"
+              v-model="variable.value"
+              placeholder="Value"
+              :type="'text'"
+            ></b-form-input>
+            <b-form-input
+              v-else-if="variable.type === 'integer'"
+              v-model="variable.value"
+              placeholder="Value"
+              :type="'number'"
+            ></b-form-input>
+            <b-form-select
+              v-else-if="variable.type === 'boolean'"
+              v-model="variable.value"
+              :options="boolOptions"
+            ></b-form-select>
+            <b-form-input :disabled="true" v-else></b-form-input>
+          </b-col>
+          <b-col cols="1">
+            <b-button @click="deleteVariable(variable.id)" variant="danger">-</b-button>
+          </b-col>
+        </b-form-row>
+      </b-container>
+
     <b-button @click="addVariable">Add Variable</b-button>
 
     <template v-slot:modal-footer="{ok, cancel}">
@@ -76,6 +81,9 @@ export default {
         type: "",
         value: ""
       });
+    },
+    deleteVariable(id) {
+      this.processVariables = this.processVariables.filter(pv => pv.id !== id);
     },
     createInstance() {
       axios
