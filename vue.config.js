b8859b7956646b7fa73c7c6be3be3aaec6408e91
jomo02 <jonas.mohr@dfki.de>
2021-06-29 14:03:33 +0200

    Use only process env variable in devServer

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,9 @@
-import getEnv from '@/helpers/env';
-
 module.exports = {
     // https://medium.com/js-dojo/how-to-deal-with-cors-error-on-vue-cli-3-d78c024ce8d3
     devServer: {
         proxy: {
             "^/engine-rest": {
-                target: getEnv('VUE_APP_CAMUNDA_REST_URL'),
+                target: process.env.VUE_APP_CAMUNDA_REST_URL,
             }
         }          
     },
