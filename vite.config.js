874e00762f131f1855eb56bc63055f74fc930f63
jomo02 <jonas.mohr@dfki.de>
2023-08-02 11:27:12 +0200

    Update Vue, Vite, Axios and UUID deps refs i40/administration#221

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ const path = require('path');
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 3000,
+    hmr: true, // enable hot module replacement
     // https://medium.com/js-dojo/how-to-deal-with-cors-error-on-vue-cli-3-d78c024ce8d3
     proxy: {
       '^/engine-rest': 'http://basys-lnv-1.mrk40.dfki.lan:9080', //import.meta.env.VITE_CAMUNDA_REST_URL,
