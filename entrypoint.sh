1c5040c83890a8f9521827326b3dcedf6054d170
jomo02 <jonas.mohr@dfki.de>
2023-12-12 11:42:02 +0100

    Set NGINX_PROXY default value if not set
    refs /i40/twinficient/twinficient-administration#27
    
    Signed-off-by: jomo02 <jonas.mohr@dfki.de>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -eu
 
+# Set NGINX_PROXY to a default value if not set
+export NGINX_PROXY="${NGINX_PROXY:-http://localhost}"
+
 envsubst '${NGINX_PROXY}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+echo "NGINX_PROXY is set to: $NGINX_PROXY"
 
 JSON_STRING='window.configs = { \
   "VITE_MQTT_BROKER_URL":"'"${VITE_MQTT_BROKER_URL}"'", \
